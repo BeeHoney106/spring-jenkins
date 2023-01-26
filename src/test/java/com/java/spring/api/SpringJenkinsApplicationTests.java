c635cc9d600dfc18d88b39e59b921f87cf59ec04
Vardan Middela <vardanm575@gmail.com>
2023-01-26 16:30:19 -0500

    Initial commit

---
+++ src/test/java/com/java/spring/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.java.spring.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	static Logger logger =
+	LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+
+	@Test
+	void contextLoads() {
+		logger.info(" TEST CASE EXECUTING ");
+		assertEquals(true,true);
+	}
+
+}
